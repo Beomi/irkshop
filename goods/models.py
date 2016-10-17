aa01ccd0172a2c1e9e7e12b551dabf936188e4b4
Beomi <latheledusjp@gmail.com>
2016-10-17 15:15:26 +0900

    update goods model

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+from datetime import date
+
+
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+
+class Goods(TimeStampModel):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField()
+    sell_until = models.DateTimeField(blank=True)
+    is_valid = models.BooleanField(default=True)
+
+    @@property
+    def is_available(self):
+        if (date.today() < self.sell_until) and (self.is_valid):
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        if self.is_available:
+            status = "구입가능"
+        else:
+            status = "구입불가"
+        return "{}()".format(
+            self.name,
+            status
+        )
+
+
+class Shipping(TimeStampModel):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    receive_at = models.CharField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return "({}){}: {} ({})".format(
+            self.name,
+            self.receive_at,
+            self.price,
+            self.country
+        )
