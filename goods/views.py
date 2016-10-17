834417529a3b33c19bb69b6df457c588cca3bff4
Beomi <latheledusjp@gmail.com>
2016-10-18 08:45:32 +0900

    add card views

--- goods/views.py
+++ goods/views.py
@@ -5,6 +5,17 @@ from django.contrib.auth.decorators import login_required
 from .models import Goods
 from .models import Shipping
 
+from carton.cart import Cart
+
 
 def index(request):
     return render(request, 'goods/index.html')
+
+def add_cart(request):
+    cart = Cart(request.session)
+    goods = Goods.objects.get(id=request.GET.get('goods_id'))
+    cart.add(goods, price=goods.price)
+    return JsonResponse("Added")
+
+def show_cart(request):
+    return render(request, 'shopping/show-cart.html')
--- irkshop/settings.py
+++ irkshop/settings.py
@@ -126,11 +126,11 @@ SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
+    'django.template.context_processors.debug',
+    'django.template.context_processors.i18n',
+    'django.template.context_processors.media',
+    'django.template.context_processors.static',
+    'django.template.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     'social.apps.django_app.context_processors.backends',
     'social.apps.django_app.context_processors.login_redirect',
