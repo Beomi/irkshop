a045b1f417e5daecfeb6679bbc82b50c820fd9bc
Beomi <latheledusjp@gmail.com>
2016-10-17 16:11:28 +0900

    let django admin page use to login

--- irkshop/settings.py
+++ irkshop/settings.py
@@ -57,8 +57,10 @@ ROOT_URLCONF = 'irkshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+        ],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
--- irkshop/urls.py
+++ irkshop/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', login,
+        {'template_name': 'admin/login.html'}),
+    url(r'^accounts/logout/$', logout),
 ]
