3527e3b2260c9e969268fc8ac49069cdc4612bd0
Beomi <latheledusjp@gmail.com>
2016-10-31 20:12:28 +0900

    not show not available products

--- goods/templatetags/in_category.py
+++ goods/templatetags/in_category.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def in_category(things, category):
     return things.filter(category=category)
+
+@register.filter
+def is_available(things, avail):
+    return things.filter(is_available=True)
